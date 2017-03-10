ba5d1d1fb1aa8e98bd64479cb1b4655e7315223a
Samson Akanbi <samson.akanbi@outlook.com>
2017-03-10 11:24:31 +0000

    Created a resource route for articles and a new template

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'plainpage#index'
    get 'form', to: 'plainpage#form'
+   
+   resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
